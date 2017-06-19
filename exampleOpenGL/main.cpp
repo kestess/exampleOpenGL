495194cdd0959d686d62941eada9b93cea7d6fe2
John Henry Estess IV <je@zerofluid.com>
2017-06-19 12:32:08 -0700

    Initial Commit

---
+++ exampleOpenGL/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  exampleOpenGL
+//
+//  Created by John Estess on 6/19/17.
+//  Copyright © 2017 John Estess. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
